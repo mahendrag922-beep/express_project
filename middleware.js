e43846ad374b683490a04853b71d8bfd907b2818
Mahendrag922 <Mahendrag922@gmial.com>
2026-01-16 01:10:57 +0530

    add varibale port and pass the value of port in app.listen

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 const addUserMiddleware = (req, res, next) => {
   req.user = "Guest";   // adding new property to req object
   next();               // move to next middleware / route
@@ -7,6 +8,6 @@ const addUserMiddleware = (req, res, next) => {
 app.get("/welcome", addUserMiddleware, (req, res) => {
   res.send(`<h1>Welcome, ${req.user}!</h1>`);
 });
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server running on http://localhost:3000");
 });
