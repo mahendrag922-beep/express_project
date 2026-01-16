19e69f3b79f46eaa1bdd55072418dbcf86dcdaad
Mahendrag922 <Mahendrag922@gmial.com>
2026-01-17 00:24:49 +0530

    add page not found error

--- routes_pr.js
+++ routes_pr.js
@@ -24,6 +24,12 @@ app.post("/users", (req, res) => {
   res.send("A new user has been added.");
 });
 
+app.use("*",(req,res)=>{
+  res.status(404).send(`
+    <h1> 404</h1>
+    <p>page not found </p>`);
+});
+
 // Start server on port 3000
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
