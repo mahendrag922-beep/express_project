f04773785ae2cd2c466ac3ccc928c560367c9051
Mahendrag922 <Mahendrag922@gmial.com>
2026-01-17 00:26:16 +0530

    minor correction

--- routes_pr.js
+++ routes_pr.js
@@ -24,7 +24,7 @@ app.post("/users", (req, res) => {
   res.send("A new user has been added.");
 });
 
-app.use("*",(req,res)=>{
+app.use((req,res)=>{
   res.status(404).send(`
     <h1> 404</h1>
     <p>page not found </p>`);
