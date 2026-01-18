22b31f1d0f2d9e79b99209d17d19c44fa57762fd
Mahendrag922 <Mahendrag922@gmial.com>
2026-01-18 22:47:15 +0530

    Add product controller

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/",(req,res)=>{
-   res.send("Fetching all products");
-});
-router.post("/",(req,res)=>{
-  res.send("Adding a new product");
-});
-router.get("/:id",(req,res)=>{
-  const id = req.params.id;
-  res.send(`Fetching product with ID: ${id}`);
-});
+const {getProduct,
+    getProductById,
+    addProduct }= require("../controllers/productController");
+  router.get("/",getProduct);
+  router.post("/",addProduct);
+  router.get("/:id",getProductById);
 module.exports=router;
